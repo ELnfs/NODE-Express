00ffee1d3eb53150ae96bc117d8bd6a2d08061f5
EL <joselezama.emilio@gmail.com>
2019-05-29 16:17:55 +0200

    index hbs

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
-console.log("terreviento");
+const exphbs = require('express-handlebars');
 const express=require('express');
 const app=express();
 
 const PORT=3000;
 
 app.listen(PORT, ()=>{
+
     console.log('Está furuling :)');
 })
+//Preparamos el entorno de plantillas Handlebars
+app.engine("handlebars",exphbs())
+app.set("view engine", "handlebars")
 app.get('/',(request,response)=>
-response.send("Hello World"))
+response.render("index"))
+
+
